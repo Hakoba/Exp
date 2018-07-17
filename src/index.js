c9aa7abdd604cc5e194e005e622ff089d824510e
Hakoba <amion980@gmail.com>
2018-07-17 16:46:19 +0300

    Add:
    -smooth scroll
    -fixed slider
    -add new section
    -add plugin scrollReveal

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import './sass/base.sass';
 //import 'bootstrap/scss/bootstrap-grid.scss';
 import $ from 'jquery';
-import cl from './js/main.js';
+import  './js/main.js';
 import img from './img/favicon.ico';
 import Rellax from 'rellax';
 import {lory} from 'lory.js';
-
+import ScrollReveal from 'scrollreveal'
 // <меню на мобильных>
 $("#toggle").click(function(){
     $(this).toggleClass('on');
@@ -20,6 +20,22 @@ $('header').mousemove(function(e){
  $('.content').css('margin-left', moveX + 'px');
  //$('.content').css('margin-top', moveX + 'px');
 })*/
+
+$(document).ready(function(){
+	$(".menu").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 800);
+	});
+});
 
 // Сворачивание <nav> при прокрутке вниз
 let prevScrollpos = window.pageYOffset;
@@ -59,3 +75,17 @@ $(window).scroll(function(){
         classNameNextCtrl: 'slider-next'
     });
 });
+/// 
+window.sr = ScrollReveal();
+sr.reveal('.scrollClass', 
+{  
+    duration: 200,
+}); 
+sr.reveal('.scrollClass2', 
+{  
+    duration: 1000,
+}); 
+sr.reveal('#sites', {
+    duration: 50,
+    
+})
